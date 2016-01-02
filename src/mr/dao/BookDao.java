37e292f17bdea7e208b6192576167ae727a84f36
huyangc <huyangc@gmail.com>
2016-01-02 15:09:38 +0800

    bookDao

---
+++ src/mr/dao/BookDao.java
@@ -0,0 +1,61 @@
+package mr.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import mr.domain.Book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDao {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public Book getById(final Long id){
+		final Book result=new Book();
+		String sqlStr="select * from book where id=?";
+		jdbcTemplate.query(sqlStr,new Object[] {id},
+				new RowCallbackHandler(){
+					@Override
+					public void processRow(ResultSet rs) throws SQLException {
+						result.setId(id);
+						result.setAuthor(rs.getString("author"));
+						result.setImgLoc(rs.getString("img_loc"));
+						result.setPageNum(rs.getInt("page_num"));
+						result.setPublisher(rs.getString("publisher"));
+						result.setTitle(rs.getString("title"));
+						result.setCatalog(rs.getString("catalog"));
+					}
+		});
+		return result;
+	}
+	
+	public List<Book> getAll(){
+		final List<Book> result = new ArrayList<Book>(); 
+		String sqlStr="select * from book";
+		jdbcTemplate.query(sqlStr,
+				new RowCallbackHandler(){
+					@Override
+					public void processRow(ResultSet rs) throws SQLException {
+						Book book = new Book();
+						book.setId(rs.getLong("id"));
+						book.setAuthor(rs.getString("author"));
+						book.setImgLoc(rs.getString("img_loc"));
+						book.setPageNum(rs.getInt("page_num"));
+						book.setPublisher(rs.getString("publisher"));
+						book.setTitle(rs.getString("title"));
+						book.setCatalog(rs.getString("catalog"));
+						book.setDa_id(rs.getInt("da_id"));
+						book.setType(rs.getInt("type"));
+						result.add(book);
+					}
+		});
+		return result;
+	}
+}
---
+++ src/mr/domain/Book.java
@@ -0,0 +1,72 @@
+package mr.domain;
+
+public class Book {
+	private long id;
+	private String title;
+	private String author;
+	private String publisher;
+	private int pageNum;
+	private String imgLoc;
+	private String catalog;
+	private int type;
+	private int da_id;
+	
+	
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public int getDa_id() {
+		return da_id;
+	}
+	public void setDa_id(int da_id) {
+		this.da_id = da_id;
+	}
+	public String getCatalog() {
+		return catalog;
+	}
+	public void setCatalog(String catelog) {
+		this.catalog = catelog;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getPageNum() {
+		return pageNum;
+	}
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+	public String getImgLoc() {
+		return imgLoc;
+	}
+	public void setImgLoc(String imgLoc) {
+		this.imgLoc = imgLoc;
+	}
+	
+	
+	
+}
