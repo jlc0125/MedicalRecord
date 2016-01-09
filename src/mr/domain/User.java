d0f74da946704dee5d10849b71d8dd847644cb57
huyangc <huyangc@gmail.com>
2016-01-09 18:46:37 +0800

    add cas support and login\out\regist logic

--- src/mr/domain/User.java
+++ src/mr/domain/User.java
@@ -4,49 +4,17 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class User implements Serializable {
-	private Long userId;
-	private String userName;
-	private String password;
-	private Integer[] collection;
-	private String lastIp;
-	private Date lastVisit;
-	
-	public Integer[] getCollection() {
-		return collection;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	public String getName() {
+		return name;
 	}
-	public void setCollection(Integer[] collection) {
-		this.collection = collection;
-	}
-	public Long getUserId() {
-		return userId;
-	}
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getLastIp() {
-		return lastIp;
-	}
-	public void setLastIp(String lastIp) {
-		this.lastIp = lastIp;
-	}
-	public Date getLastVisit() {
-		return lastVisit;
-	}
-	public void setLastVisit(Date lastVisit) {
-		this.lastVisit = lastVisit;
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
--- test/mr/service/TestUserService.java
+++ test/mr/service/TestUserService.java
@@ -18,27 +18,4 @@ public class TestUserService {
 	@Autowired
     private UserService userService;
 
-	@Test
-	public void testHasMatchUser() {
-		boolean b1 = userService.hasMatchUser("admin", "123456");
-		boolean b2 = userService.hasMatchUser("admin", "1111");
-		assertTrue(b1);
-		assertTrue(!b2);
-	}
-
-	@Test
-	public void testFindUserByUserName() {
-		User user = userService.findUserByUserName("admin");
-		assertEquals(user.getUserName(), "admin");
-	}
-
-	@Test
-	public void testAddLoginLog() {
-		User user = userService.findUserByUserName("admin");
-		user.setUserId(1L);
-		user.setUserName("admin");
-		user.setLastIp("192.168.12.7");
-		user.setLastVisit(new Date());
-		userService.loginSuccess(user);
-	}
 }
